ac46e871b22f2ce65b53935b577f6245038048f9
League Student <league@iMac-24.attlocal.net>
2019-11-20 18:26:07 -0800

    good

---
+++ src/_02_unbirthday/Unbirthday.java
@@ -0,0 +1,16 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	String birthday= JOptionPane.showInputDialog(null, "WHEN IIIIIIS UR BIRTHDAY BRUH?!?!?!");
+	if(birthday.equals("11/20")) {
+		JOptionPane.showMessageDialog(null, "WELL HAPPY BIRTHDAY!!!!!!!!!!!");
+			
+}
+	else {
+		JOptionPane.showMessageDialog(null, "HAPPY UNBIRTHDAY!!!!!!!!!!!!!!!!!");
+	}
+}
+}
--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -7,12 +7,18 @@ import javax.swing.JOptionPane;
 public class Remarkable {
 public static void main(String[] args) {
 
-		// 1. Save something remarkable about each person in a variable.
-
+		// 1. Save something remarkable about each person in a variable
+	String pot= "u great"; 
+	String rock="u funny";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+String name= JOptionPane.showInputDialog(null,"WHAT IS UR NAME?!!!!! TELL MEH IMMEDIATELY!!!!!!!!!!!!!!");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+if(name.equals("pot")){
+	JOptionPane.showMessageDialog(null, pot);
+}
+else {
+	JOptionPane.showMessageDialog(null, rock);
+}
 	}
 }
 
